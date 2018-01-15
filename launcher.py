d54b2a02834b46429252880e313c12a3218713fd
Steve <stevenjmesser@gmail.com>
2018-01-15 22:15:48 +0000

    Light sequence as function, pass colour and time as args

--- launcher.py
+++ launcher.py
@@ -5,44 +5,53 @@ import buttonshim
 import pimodoro
 import random
 import time
-
-from blinkt import set_pixel, show, clear
+import blinkt
 from subprocess import call
 
 buttonshim.set_pixel(0, 0, 0)
 
 # Random lights will blink when first launched.
-r = random.randint(0, 255)
-g = random.randint(0, 255)
-b = random.randint(0, 255)
+rand_r = random.randint(0, 255)
+rand_g = random.randint(0, 255)
+rand_b = random.randint(0, 255)
 
 for x in range(16):
     pixels = random.sample(range(8), random.randint(1, 5))
     for i in range(8):
         if i in pixels:
-            set_pixel(i, r, g, b)
+            blinkt.set_pixel(i, rand_r, rand_g, rand_b)
         else:
-            set_pixel(i, 0, 0, 0)
-    show()
+            blinkt.set_pixel(i, 0, 0, 0)
+    blinkt.show()
     time.sleep(0.05)
 
-clear()
-show()
+blinkt.clear()
+blinkt.show()
+
+# Set Pomodoro, short break and long break lengths in minutes. We multiply by 60 to get seconds.
+pomodoro_length = 25 * 60
+shortbreak_length = 2 * 60
+longbreak_length = 7 * 60
+
+# Stepdown times to fade the lights.
+pom_fade = (pomodoro_length / 8) / 10
+sb_fade = (shortbreak_length / 8) / 10
+lb_fade = (longbreak_length / 8) / 10
 
 # Press button A to start a pomodoro.
 @buttonshim.on_press(buttonshim.BUTTON_A)
 def button_a(button, pressed):
-    pimodoro.pomodoro()
+    pimodoro.lightsequence(255, 0, 0, pom_fade)
 
 # Press button B to start a short break.
 @buttonshim.on_press(buttonshim.BUTTON_B)
 def button_b(button, pressed):
-    pimodoro.shortbreak()
+    pimodoro.lightsequence(0, 255, 0, sb_fade)
 
 # Press button C to start a long break.
 @buttonshim.on_press(buttonshim.BUTTON_C)
 def button_c(button, pressed):
-    pimodoro.longbreak()
+    pimodoro.lightsequence(0, 0, 255, lb_fade)
 
 # Press button E for to shut down.
 @buttonshim.on_press(buttonshim.BUTTON_E)
--- pimodoro.py
+++ pimodoro.py
@@ -7,43 +7,11 @@ import numpy as np
 
 blinkt.clear()
 
-# Set Pomodoro, short break and long break lengths in minutes below. Multiply by 60.
-
-pom_len = 25 * 60
-sb_len = 2 * 60
-lb_len = 7 * 60
-
-# Stepdown times to fade the lights.
-pom_fade = (pom_len / 8) / 10
-sb_fade = (sb_len / 8) / 10
-lb_fade = (lb_len / 8) / 10
-
-# A test fade to demo the sequence.
-test_fade = 0.05
-
-# A Pomodoro sequence.
-def pomodoro():
-    blinkt.set_all(255, 12, 0, brightness=0.1)
+# A light sequence.
+def lightsequence(r, g, b, fade_time):
+    blinkt.set_all(r, g, b, brightness=0.1)
     for x in range(7, -1, -1):
         for i in np.arange(0.1, 0, -0.01):
-            blinkt.set_pixel(x, 255, 12, 0, brightness=i)
+            blinkt.set_pixel(x, r, g, b, brightness=i)
             blinkt.show()
-            time.sleep(pom_fade)
-
-# A short break sequence.
-def shortbreak():
-    blinkt.set_all(71, 255, 71, brightness=0.1)
-    for x in range(7, -1, -1):
-        for i in np.arange(0.1, 0, -0.01):
-            blinkt.set_pixel(x, 71, 255, 71, brightness=i)
-            blinkt.show()
-            time.sleep(sb_fade)
-
-# A long break sequence.
-def longbreak():
-    blinkt.set_all(99, 71, 255, brightness=0.1)
-    for x in range(7, -1, -1):
-        for i in np.arange(0.1, 0, -0.01):
-            blinkt.set_pixel(x, 99, 71, 255, brightness=i)
-            blinkt.show()
-            time.sleep(lb_fade)
+            time.sleep(fade_time)
